4b7f62c5fe94df065d4acb70507892933578dd63
TJ Draper <tj@buzzingpixel.com>
2021-10-01 22:13:57 -0500

    Add data sources and update config

--- config/general.php
+++ config/general.php
@@ -8,6 +8,25 @@
  * @see \craft\config\GeneralConfig
  */
 
+// \yii\base\Event::on(
+//     \BuzzingPixel\CraftScheduler\CraftSchedulerPlugin::class,
+//     \BuzzingPixel\CraftScheduler\CraftSchedulerPlugin::EVEN_SET_DEFAULT_CONTAINER,
+//     function (\BuzzingPixel\CraftScheduler\ScheduleRetrieval\SetDefaultContainerEvent $e) {
+//         $e->setDefaultContainer('asdf');
+//     }
+// );
+
+// \yii\base\Event::on(
+//     \BuzzingPixel\CraftScheduler\ScheduleRetrieval\RetrieveSchedule::class,
+//     \BuzzingPixel\CraftScheduler\ScheduleRetrieval\RetrieveSchedule::EVENT_RETRIEVE_SCHEDULE,
+//     static function (\BuzzingPixel\CraftScheduler\ScheduleRetrieval\RetrieveScheduleEvent $e): void {
+//         $e->scheduleConfigItems()->addItem(item: new \BuzzingPixel\CraftScheduler\ScheduleRetrieval\ScheduleConfigItem::__construct(
+//             TestClass::class,
+//             \BuzzingPixel\CraftScheduler\Frequency::ALWAYS,
+//         ));
+//     }
+// );
+
 return [
     'defaultWeekStartDay' => 1,
     'omitScriptNameInUrls' => true,
